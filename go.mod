d51a779acf9464e919279a09d77e88ff7d29ce7a
Tshaka Lekholoane <mail+git@tshaka.dev>
2024-11-15 13:59:13 +0200

    fix: err on invalid flags
    (Amongst other things).

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module tshaka.dev/x/bat
 
 go 1.21
 
-require (
-	golang.org/x/sys v0.27.0
-	tshaka.dev/x/diff v0.1.0
-)
+require golang.org/x/sys v0.27.0
--- main_linux.go
+++ main_linux.go
@@ -5,228 +5,291 @@ import (
 	"bytes"
 	_ "embed"
 	"errors"
+	"flag"
 	"fmt"
-	"io"
 	"io/fs"
 	"os"
 	"os/exec"
-	"path"
 	"path/filepath"
-	"runtime/debug"
+	rtdebug "runtime/debug"
 	"strconv"
-	"strings"
-	"syscall"
 	"text/template"
 
 	"golang.org/x/sys/unix"
 )
 
-const (
-	device    = "/sys/class/power_supply/BAT?"
-	threshold = "charge_control_end_threshold"
-	services  = "/etc/systemd/system/"
-)
-
-var events = [...]string{
-	"hibernate",
-	"hybrid-sleep",
-	"multi-user",
-	"suspend",
-	"suspend-then-hibernate",
+type Service struct {
+	Event, Path, Shell string
+	Threshold          int
 }
 
-var build, tag string
+const threshold = "charge_control_end_threshold"
 
 var (
+	tag string
+
+	events = [...]string{
+		"hibernate",
+		"hybrid-sleep",
+		"multi-user",
+		"suspend",
+		"suspend-then-hibernate",
+	}
+
+	services = filepath.Join("/", "etc", "systemd", "system")
+
 	//go:embed bat.service
 	unit string
+
 	//go:embed help.txt
-	help string
-	//go:embed version.fmt
-	version string
+	usage string
 )
 
-func exists(battery, variable string) bool {
-	_, err := os.Stat(filepath.Join(battery, variable))
-	if err != nil && errors.Is(err, fs.ErrNotExist) {
-		return false
-	}
-	return true
+type battery struct {
+	root string
 }
 
-func must[T any](v T, err error) T {
+func (b *battery) has(variable string) (bool, error) {
+	_, err := os.Stat(filepath.Join(b.root, variable))
 	if err != nil {
-		panic(err)
+		if errors.Is(err, fs.ErrNotExist) {
+			return false, nil
+		}
+		return false, err
 	}
-	return v
+	return true, nil
 }
 
-func read(battery, variable string) (string, error) {
-	f, err := os.Open(filepath.Join(battery, variable))
+func (b *battery) path(variable string) string {
+	return filepath.Join(b.root, variable)
+}
+
+func (b *battery) read(variable string) (string, error) {
+	contents, err := os.ReadFile(b.path(variable))
 	if err != nil {
 		return "", err
 	}
-	defer f.Close()
-	buf := make([]byte, 32)
-	n, err := f.Read(buf)
-	if err != nil && err != io.EOF {
-		return "", err
-	}
-	return string(buf[:n-1]), nil
+	return string(bytes.TrimSpace(contents)), nil
 }
 
-type options struct{ debug, help, version bool }
+func (b *battery) write(variable string, contents []byte) error {
+	return os.WriteFile(b.path(variable), contents, 0o644)
+}
 
 func main() {
-	var opts options
-	for _, arg := range os.Args {
-		switch arg {
-		case "-d", "--debug":
-			opts.debug = true
-		case "-h", "--help":
-			opts.help = true
-		case "-v", "--version":
-			opts.version = true
-		}
+	// Default flag.Usage is overridden below.
+	const ignore = ""
+	var (
+		d, debug   = flag.Bool("d", false, ignore), flag.Bool("debug", false, ignore)
+		h, help    = flag.Bool("h", false, ignore), flag.Bool("help", false, ignore)
+		v, version = flag.Bool("v", false, ignore), flag.Bool("version", false, ignore)
+	)
+	flag.Usage = func() {
+		fmt.Print(usage)
 	}
+	flag.Parse()
 
-	if len(os.Args) == 1 {
-		fmt.Fprint(os.Stderr, help)
-		os.Exit(2)
-	}
-
-	if opts.help {
-		fmt.Print(help)
+	if *h || *help {
+		flag.Usage()
 		return
 	}
 
-	if opts.version {
-		fmt.Printf(version, tag)
+	if *v || *version {
+		fmt.Printf("bat %s\nCopyright (c) 2021 Tshaka Lekholoane.\nMIT Licence.\n", tag)
 		return
 	}
 
 	defer func() {
 		if err := recover(); err != nil {
-			if opts.debug {
-				fmt.Fprintf(os.Stderr, "%s\n\n%s", err, string(debug.Stack()))
+			var message string
+			if *d || *debug {
+				message = fmt.Sprintf("%s\n\n%s", err, string(rtdebug.Stack()))
 			} else {
-				fmt.Println("A fatal error occurred. Please rerun the command with the `--debug` flag enabled\nand file an issue with the resulting output at the following address\nhttps://github.com/tshakalekholoane/bat/issues/new.")
+				message = "A fatal error occurred. Please rerun the command with the `--debug` flag\n" +
+					"enabled, and file an issue with the resulting output to the following address:\n" +
+					"https://github.com/tshakalekholoane/bat/issues/new."
 			}
+			fmt.Fprintln(os.Stderr, message)
 		}
 	}()
 
-	batteries := must(filepath.Glob(device))
-	if len(batteries) == 0 {
-		fmt.Fprintln(os.Stderr, "This program is most likely not compatible with your system. See\nhttps://github.com/tshakalekholoane/bat#disclaimer for details.")
-		os.Exit(1)
+	if flag.NArg() == 0 {
+		flag.Usage()
+		os.Exit(2)
 	}
 
-	// Default to using the first.
-	bat := batteries[0]
+	batteries, err := filepath.Glob(filepath.Join("/", "sys", "class", "power_supply", "BAT?"))
+	if err != nil {
+		panic(err)
+	}
+	if len(batteries) == 0 {
+		fmt.Fprintln(
+			os.Stderr,
+			"This program is most likely not compatible with your system. See\n"+
+				"https://github.com/tshakalekholoane/bat#disclaimer for details.",
+		)
+		os.Exit(1)
+	}
+	// Default to using the first battery.
+	bat := &battery{root: batteries[0]}
 
-	switch variable := os.Args[1]; variable {
+	switch subcommand := flag.Arg(0); subcommand {
 	case "capacity", "status":
-		fmt.Println(must(read(bat, variable)))
+		v, err := bat.read(subcommand)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(v)
 	case "health":
 		// Some devices use charge_* and others energy_* so probe both. The
-		// health is computed as v / w where v is the eroded capacity and w
+		// health is computed as x / y where x is the eroded capacity and y
 		// is the capacity when the battery was new.
-		var enoent bool
-		x, y := "charge_full", "charge_full_design"
-		_, err := os.Stat(filepath.Join(bat, x))
+		var (
+			err  error
+			v, w string
+		)
+		s, t := "charge_full", "charge_full_design"
+		v, err = bat.read(s)
 		if err != nil {
-			if !errors.Is(err, fs.ErrNotExist) {
-				panic(err)
+			if errors.Is(err, fs.ErrNotExist) {
+				goto energy
 			}
-			enoent = true
+			panic(err)
 		}
-		_, err = os.Stat(filepath.Join(bat, y))
+		w, err = bat.read(t)
+		if err != nil && !errors.Is(err, fs.ErrNotExist) {
+			panic(err)
+		}
+		goto health
+	energy:
+		// Should have one or the other.
+		s, t = "energy_full", "energy_full_design"
+		v, err = bat.read(s)
 		if err != nil {
-			if !errors.Is(err, fs.ErrNotExist) {
-				panic(err)
-			}
-			enoent = true
+			panic(err)
 		}
-		if enoent {
-			x, y = "energy_full", "energy_full_design"
+		w, err = bat.read(t)
+		if err != nil {
+			panic(err)
 		}
-		v := must(strconv.Atoi(must(read(bat, x))))
-		w := must(strconv.Atoi(must(read(bat, y))))
-		fmt.Println(v * 100 / w)
+	health:
+		x, err := strconv.Atoi(v)
+		if err != nil {
+			panic(err)
+		}
+		y, err := strconv.Atoi(w)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(x * 100 / y)
 	case "persist":
-		if !exists(bat, threshold) {
-			fmt.Fprintf(os.Stderr, "Charging threshold setting not found.\n")
+		ok, err := bat.has(threshold)
+		if err != nil {
+			panic(err)
+		}
+		if !ok {
+			fmt.Fprintln(os.Stderr, "Charging threshold setting not found.")
 			os.Exit(1)
 		}
+
 		// systemd 244-rc1 is the earliest version to allow restarts for
 		// oneshot services.
-		out := must(exec.Command("systemctl", "--version").CombinedOutput())
-		var rev int
-		_ = must(fmt.Sscanf(string(out), "systemd %d", &rev))
-		if rev < 244 {
+		output, err := exec.Command("systemctl", "--version").CombinedOutput()
+		if err != nil {
+			panic(err)
+		}
+		var revision int
+		_, err = fmt.Sscanf(string(output), "systemd %d", &revision)
+		if err != nil {
+			panic(err)
+		}
+		if revision < 244 {
 			fmt.Fprintln(os.Stderr, "Requires systemd version 243-rc1 or later.")
 			os.Exit(1)
 		}
 
-		curr := must(strconv.Atoi(must(read(bat, threshold))))
-		sh, err := exec.LookPath("sh")
+		shell, err := exec.LookPath("sh")
 		if err != nil {
 			if errors.Is(err, exec.ErrNotFound) {
-				fmt.Fprintln(os.Stderr, "Could not find 'sh' on your system.")
+				fmt.Fprintln(os.Stderr, "Could not find `sh` in your `$PATH`.")
 				os.Exit(1)
 			}
 			panic(err)
 		}
-		p := path.Join(bat, threshold)
+
+		v, err := bat.read(threshold)
+		if err != nil {
+			panic(err)
+		}
+		current, err := strconv.Atoi(v)
+		if err != nil {
+			panic(err)
+		}
+
 		tmpl := template.Must(template.New("unit").Parse(unit))
 		for _, event := range events {
-			name := services + "bat-" + event + ".service"
-			f, err := os.Create(name)
+			service := "bat-" + event + ".service"
+			f, err := os.Create(filepath.Join(services, service))
 			if err != nil {
-				if errors.Is(err, syscall.EACCES) {
-					fmt.Fprintln(os.Stderr, "Permission denied. Try running this command using sudo.")
+				if errors.Is(err, unix.EACCES) {
+					fmt.Fprintln(os.Stderr, "Permission denied. Try running this command with `sudo`.")
 					os.Exit(1)
 				}
 				panic(err)
 			}
-			defer f.Close()
-			svc := struct {
-				Event, Path, Shell string
-				Threshold          int
-			}{event, p, sh, curr}
-			if err := tmpl.Execute(f, svc); err != nil {
+			s := Service{
+				Event:     event,
+				Path:      bat.path(threshold),
+				Shell:     shell,
+				Threshold: current,
+			}
+			if err = tmpl.Execute(f, s); err != nil {
 				panic(err)
 			}
-			if err := exec.Command("systemctl", "enable", name).Run(); err != nil {
+			if err := exec.Command("systemctl", "enable", service).Run(); err != nil {
 				panic(err)
 			}
+			f.Close()
 		}
 		fmt.Println("Persistence of the current charging threshold enabled.")
 	case "threshold":
-		if !exists(bat, threshold) {
-			fmt.Fprintf(os.Stderr, "Charging threshold setting not found.\n")
+		ok, err := bat.has(threshold)
+		if err != nil {
+			panic(err)
+		}
+		if !ok {
+			fmt.Fprintln(os.Stderr, "Charging threshold setting not found.")
 			os.Exit(1)
 		}
-		if len(os.Args) == 2 || len(os.Args) == 3 && opts.debug {
+		switch flag.NArg() {
+		case 1:
 			// Get.
-			fmt.Println(must(read(bat, threshold)))
-		} else {
+			var v string
+			v, err = bat.read(threshold)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(v)
+		case 2:
 			// Set.
 			// The earliest version of the Linux kernel to expose the battery
 			// charging threshold is 5.4.
 			var utsname unix.Utsname
-			if err := unix.Uname(&utsname); err != nil {
+			if err = unix.Uname(&utsname); err != nil {
 				panic(err)
 			}
 			var maj, min int
-			_ = must(fmt.Sscanf(string(utsname.Release[:]), "%d.%d", &maj, &min))
+			_, err = fmt.Sscanf(string(utsname.Release[:]), "%d.%d", &maj, &min)
+			if err != nil {
+				panic(err)
+			}
 			if maj <= 5 && (maj != 5 || min < 4) {
-				fmt.Fprintf(os.Stderr, "Requires Linux kernel version 5.4 or later.")
+				fmt.Fprintln(os.Stderr, "Requires Linux kernel version 5.4 or later.")
 				os.Exit(1)
 			}
 
-			a := os.Args[2]
-			i, err := strconv.Atoi(a)
+			setting := flag.Arg(1)
+			i, err := strconv.Atoi(setting)
 			if err != nil {
 				if errors.Is(err, strconv.ErrSyntax) {
 					fmt.Fprintln(os.Stderr, "Argument should be an integer.")
@@ -238,36 +301,42 @@ func main() {
 				fmt.Fprintln(os.Stderr, "Threshold value should be between 1 and 100.")
 				os.Exit(1)
 			}
-
-			if err := os.WriteFile(filepath.Join(bat, threshold), []byte(a), 0o644); err != nil {
-				if errors.Is(err, syscall.EACCES) {
-					fmt.Fprintln(os.Stderr, "Permission denied. Try running this command using sudo.")
+			if err := bat.write(threshold, []byte(setting)); err != nil {
+				if errors.Is(err, unix.EACCES) {
+					fmt.Fprintln(os.Stderr, "Permission denied. Try running this command with `sudo`.")
 					os.Exit(1)
 				}
 				panic(err)
 			}
-			fmt.Println("Charging threshold set.\nRun `sudo bat persist` to persist the setting between restarts.")
+			fmt.Println("Charging threshold set.\n" +
+				"Run `sudo bat persist` to persist the setting between restarts.")
+		default:
+			fmt.Fprintln(os.Stderr, "Invalid number of arguments.")
+			flag.Usage()
+			os.Exit(1)
 		}
 	case "reset":
 		for _, event := range events {
-			name := services + "bat-" + event + ".service"
-			buf := new(bytes.Buffer)
-			cmd := exec.Command("systemctl", "disable", name)
-			cmd.Stderr = buf
-			if err := cmd.Run(); err != nil {
-				switch message := buf.String(); {
-				case strings.Contains(message, "does not exist"):
-					continue
-				case strings.Contains(message, "Access denied"):
-					fmt.Fprintln(os.Stderr, "Permission denied. Try running this command using sudo.")
+			service := "bat-" + event + ".service"
+			output, err := exec.Command("systemctl", "disable", service).CombinedOutput()
+			if err != nil {
+				// WORKAROUND: systemd returns the generic exit code 1 for all
+				// failures, so triage using a substring search on the output.
+				// This method may be unreliable in non-EN locales.
+				switch {
+				case bytes.Contains(output, []byte("authentication required")):
+					fmt.Fprintln(os.Stderr, "Permission denied. Try running this command with `sudo`.")
 					os.Exit(1)
+				case bytes.Contains(output, []byte("service does not exist")):
+					continue
 				default:
-					panic(err)
+					panic(string(output))
 				}
 			}
-			if err := os.Remove(name); err != nil && !errors.Is(err, syscall.ENOENT) {
-				if errors.Is(err, syscall.EACCES) {
-					fmt.Fprintln(os.Stderr, "Permission denied. Try running this command using sudo.")
+			err = os.Remove(filepath.Join(services, service))
+			if err != nil && !errors.Is(err, unix.ENOENT) {
+				if errors.Is(err, unix.EACCES) {
+					fmt.Fprintln(os.Stderr, "Permission denied. Try running this command with `sudo`.")
 					os.Exit(1)
 				}
 				panic(err)
@@ -275,7 +344,11 @@ func main() {
 		}
 		fmt.Println("Charging threshold persistence reset.")
 	default:
-		fmt.Fprintf(os.Stderr, "There is no %s command. Run `bat --help` to see a list of available commands.\n", variable)
+		fmt.Fprintf(
+			os.Stderr,
+			"There is no `%s` command. Run `bat --help` to see a list of available commands.\n",
+			subcommand,
+		)
 		os.Exit(1)
 	}
 }
